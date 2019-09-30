f820eb61a042a7474273f435aa71b28e2961e43f
Elisa <38461451+elisa-mamolo@users.noreply.github.com>
2019-09-30 11:27:09 +0200

    init commit

---
+++ Controllers/ContactFormSurfaceController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+using AarhusWebDevCoop.ViewModels;
+using System.Net.Mail;
+using Umbraco.Core;
+using Umbraco.Core.Models;
+using Umbraco.Core.Services;
+using Umbraco.Core.Composing.CompositionExtensions;
+
+namespace AarhusWebDevCoop.Controllers
+{
+    public class ContactFormSurfaceController: SurfaceController{
+        // GET: Default
+        public ActionResult Index(){
+            return PartialView("ContactForm", new ContactForm());
+        }
+
+        [HttpPost]
+        public ActionResult HandleFormSubmit(ContactForm model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return CurrentUmbracoPage();
+            }// Read data from model and send mail
+            TempData["success"] = true;
+
+            MailMessage message = new MailMessage();
+            message.To.Add("elisa.mamolo@gmail.com");
+            message.Subject = model.Subject;
+            message.From = new MailAddress(model.Email, model.Name);
+            message.Body = model.Message;
+            using (SmtpClient smtp = new SmtpClient())
+            {
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtp.UseDefaultCredentials = false;
+                smtp.EnableSsl = true;
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 587;
+                smtp.Credentials = new System.Net.NetworkCredential("elisa.mamolo@gmail.com", "ocdfjuutuohpfbxr");        // send mail    
+                smtp.Send(message);
+            }
+
+            //get the GuildUdi of the cuttent page
+            GuidUdi currentPageUdi = new GuidUdi(CurrentPage.ContentType.ItemType.ToString(), CurrentPage.Key);
+            //create the new content type
+            IContent msg = Services.ContentService.CreateContent(model.Subject, currentPageUdi, "message");
+            msg.SetValue("messageName", model.Name);
+            msg.SetValue("email", model.Email);
+            msg.SetValue("subject", model.Subject);
+            msg.SetValue("messageContent", model.Message);
+            msg.SetValue("umbracoNaviHide", true);
+
+            //save
+            Services.ContentService.Save(msg);
+            return RedirectToCurrentUmbracoPage();
+        }
+
+         
+   
+        }
+
+    
+}
---
+++ ViewModels/ContactForm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AarhusWebDevCoop.ViewModels
+{
+    public class ContactForm
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter your email address")]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Please enter a subject")]
+        public string Subject { get; set; }
+        [Required(ErrorMessage = "Please enter a message")]
+        public string Message { get; set; }
+    }
+}
+
+
+
